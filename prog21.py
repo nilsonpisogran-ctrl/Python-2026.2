400054db9b8235bd4e0fdbf56f180b6505958042
Nilsonpisogran-ctrl <nilsonpisogan@gmail.com>
2026-07-06 20:50:30 -0300

    prog21.py novo

---
+++ prog21.py
@@ -0,0 +1,23 @@
+# Digitar 2 valores e usando if verificar qual o maior
+
+
+while True:
+
+ print("\n===== IMPRIMIR O MAIOR MUMERO DIGITADO =====")
+
+
+n1 = int(input("Informe 1º Numero: "))
+n2 = int(input("Informe 2º Numero: "))
+
+if n1 > n2:
+    print (f"{n1} é maior que {n2}")
+elif n2 > n1:
+    print (f"{n2} é maior que {n1}")
+
+while opcao not in ("S", "N"):
+        opcao = input("Digite apenas S ou N: ").upper()
+
+#if opcao == "N":
+#   break
+
+print("\nPrograma encerrado.")
