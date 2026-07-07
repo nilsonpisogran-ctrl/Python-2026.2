7b853c8683ddc4463636b52d30a19f77e1fe7a68
Nilsonpisogran-ctrl <nilsonpisogan@gmail.com>
2026-07-06 21:00:21 -0300

    prog21.py

--- prog21.py
+++ prog21.py
@@ -1,9 +1,13 @@
 # Digitar 2 valores e usando if verificar qual o maior
 
+# git add "nomeprograma.py"
+# git commit -m  "nomeprograma.py"
+# git push -u origin main
 
-while True:
 
- print("\n===== IMPRIMIR O MAIOR MUMERO DIGITADO =====")
+#while True:
+
+print("\n===== IMPRIMIR O MAIOR MUMERO DIGITADO =====")
 
 
 n1 = int(input("Informe 1º Numero: "))
@@ -14,8 +18,8 @@ if n1 > n2:
 elif n2 > n1:
     print (f"{n2} é maior que {n1}")
 
-while opcao not in ("S", "N"):
-        opcao = input("Digite apenas S ou N: ").upper()
+#while opcao not in ("S", "N"):
+#        opcao = input("Digite apenas S ou N: ").upper()
 
 #if opcao == "N":
 #   break
